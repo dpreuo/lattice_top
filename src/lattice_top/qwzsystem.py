3cbbd1f93e5b86a343faee3373bdb5f5e562dfc2
Peru d'Ornellas <peru.dornellas@gmail.com>
2021-05-17 13:53:49 +0100

    added cmap for indices

--- src/lattice_top/qwzsystem.py
+++ src/lattice_top/qwzsystem.py
@@ -190,6 +190,14 @@ class QWZ_System(Lattice_System):
         plt.colorbar()
         plt.show()
 
+    def cmap_index(self, index, name=None):
+        Z = index.reshape((self._lengths[1], self._lengths[0]))
+        plt.pcolor(Z)
+        if name is not None:
+            plt.title(name)
+        plt.colorbar()
+        plt.show()
+
     def plot_index(self, index, name=None):
         Z = index.reshape((self._lengths[1], self._lengths[0]))
         plot_surface_3d(Z, self._lengths, title=name, xy_labels=('x', 'y'))
